5b112f542fa39e0b36b64df9053c53d1134f70a7
ZdaniuManiu <kuba2007-90-1990@o2.pl>
2025-04-22 13:55:25 +0200

    Kontakt naprawde wysyla wiadomosc

--- public/kontakt.js
+++ public/kontakt.js
@@ -1,16 +1,36 @@
-function wyslij(){
-    const imie = document.getElementById("imie");
-    const wiadomosc = document.getElementById("wiadomosc");
-    if(imie.value===""){
-        alert("Podanie swojego imienia jest -> !WYMAGANE! <-");
-    return;
-    }
-    if(wiadomosc.value===""){
-        alert("Podanie swojej wiadomosci jest -> !WYMAGANE! <-");
-        return;
-    }
-    alert("Formularz został poprawnie wypełniony, wiadomość została wysłana. ");
+// Sprawdzanie, czy wiadomość nie jest pusta
+const wiadomoscInput = document.getElementById("wiadomosc");
+const button = document.getElementById("button");
 
-    imie.value=""
-    wiadomosc.value=""
-}  
+function CzyWypelnione(){
+wiadomoscInput.addEventListener("input", function() {
+    if (wiadomoscInput.value.trim() !== "") {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+});
+}
+
+// Funkcja do walidacji formularza
+function wyslij(event) {
+const imie = document.getElementById("imie");
+const wiadomosc = document.getElementById("wiadomosc");
+
+// Sprawdzanie, czy pole email i wiadomość są wypełnione
+    if (imie.value === "") {
+        alert("Podanie swojego emaila jest -> !WYMAGANE! <-");
+    return;
+}
+    if (wiadomosc.value === "") {
+        alert("Podanie swojej wiadomości jest -> !WYMAGANE! <-");
+    return;
+}
+
+// Jeśli wszystko jest OK, wysyłamy formularz
+document.getElementById("formularz").submit();
+}
+
+// Ustawiamy nasłuchiwacz na formularz
+document.getElementById("formularz").addEventListener("submit", wyslij);
+CzyWypelnione();
